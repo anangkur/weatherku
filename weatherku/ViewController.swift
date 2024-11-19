7a32ba405bb65b695b3da4e66f57a99029ad5509
Anang Kurniawan <anangk97@gmail.com>
2024-11-19 23:39:41 +0700

    Initial Commit

---
+++ weatherku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherku
+//
+//  Created by Anang Kurniawan on 19/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ weatherkuTests/weatherkuTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherkuTests.swift
+//  weatherkuTests
+//
+//  Created by Anang Kurniawan on 19/11/24.
+//
+
+import Testing
+@testable import weatherku
+
+struct weatherkuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
